189ccaf21b3feb1828bb27514750d73074c526a8
Teable <blukey842unluck@gmail.com>
2018-12-05 07:39:28 +0000

    Add 'TimedMotion'service

--- test/travis_test_motors2.py
+++ test/travis_test_motors2.py
@@ -5,11 +5,13 @@ import time
 from pimouse_ros.msg import MotorFreqs 
 from geometry_msgs.msg import Twist 
 from std_srvs.srv import Trigger, TriggerResponse
+from pimouse_ros.srv import TimedMotion
 
 class MotorTest(unittest.TestCase):
 	def setUp(self):
 		rospy.wait_for_service('/motor_on')
 		rospy.wait_for_service('/motor_off')
+		rospy.wait_for_service('/timed_motion')
 		on = rospy.ServiceProxy('/motor_on', Trigger)
 		ret = on()
 
@@ -46,6 +48,13 @@ class MotorTest(unittest.TestCase):
 		time.sleep(1.1)
 		self.file_check("rtmotor_raw_l0",0,"don't stop after 1[s]")
 		self.file_check("rtmotor_raw_r0",0,"don't stop after 1[s]")
+
+	def test_put_value_timed(self):
+		tm = rospy.ServiceProxy('/timed_motion', TimedMotion)
+		tm(-321,654,1500)
+		with open("/dev/rtmotor0","r") as f:
+			data = f.readline()
+			self.assertEqual(data,"-321 654 1500\n","value does not written to rtmotor0")
 
 	def test_on_off(self):
 		off = rospy.ServiceProxy('/motor_off', Trigger)
